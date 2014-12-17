e68221a5fdef5b90d59f5d0f5e0875b5ce64e5c6
JuliznOo <julien.bedier@gmail.com>
2014-12-17 11:02:42 +0100

    GROSE PUTE

--- selection.php
+++ selection.php
@@ -3,4 +3,18 @@
 $stma = $pdo->prepare("SELECT * FROM participants");
 $stma->execute();
 $resultat = $stma->fetchAll();
-print_r($resultat);
+var_dump($resultat);
+$melangeMoiCa = array();
+$enleveMoiCa = array();
+foreach ($resultat as $r){
+	$melangeMoiCa[] = $r['id'];
+	$enleveMoiCa[] = $r['id_cadeau'];
+}
+$melangeMoiCa = array_diff($melangeMoiCa, $enleveMoiCa);
+shuffle($melangeMoiCa);
+$id_cadeau = $melangeMoiCa[0];
+if($pdo->lastInsertId() != $id_cadeau){
+	$id = $pdo->lastInsertId();
+	$stma = $pdo->prepare("INSERT INTO participants (id_cadeau) VALUES (".$id_cadeau.") WHERE id=".$id);
+	$stma->execute();
+}
