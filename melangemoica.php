566d5ee390ebaf771974c522f59181dcc2b10e9a
caloudinou <pascalou@gmail.com>
2014-12-17 17:30:28 +0100

    XOXO

--- melangemoica.php
+++ melangemoica.php
@@ -1,7 +1,18 @@
 <?php
 //new code julien \/
+try{
 $melangeMoiCa = array();
 $enleveMoiCa = array();
+
+
+
+//préparation de la requete
+$stmta = $pdo->prepare("
+                UPDATE participants 
+                SET (id_cadeau=:id_cadeau) 
+                WHERE id=:id
+                ");
+/*
 foreach ($resultat as $r){
 	$melangeMoiCa[] = $r['id'];
 	$enleveMoiCa[] = $r['id_cadeau'];
@@ -10,15 +21,21 @@ $melangeMoiCa = array_diff($melangeMoiCa, $enleveMoiCa);
 shuffle($melangeMoiCa);
 $id_cadeau = $melangeMoiCa[0];
 if($pdo->lastInsertId() != $id_cadeau){
-	$id = $pdo->lastInsertId();
-	$stma = $pdo->prepare("
-                UPDATE participants 
-                SET (id_cadeau=:id_cadeau) 
-                
-                ");
+	$id = $pdo->lastInsertId();*/
         
         
-       $resultat2= $stma->execute(array(
-           'id_cadeau' =>     '2'
+       $stmta->execute(array(
+           'id_cadeau' =>     1,
+           'id' =>     2
        ));
+       
+       $resultat2 = $stmta->fetchAll();
+       
+       var_dump($resultat2);
+       
+
+    
+} catch (Exception $e)
+{
+    
 }
