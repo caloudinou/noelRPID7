d0c65e62eb4875b83567a695e0e1a7aba85f7a88
caloudinou <pascalou@gmail.com>
2014-12-17 09:54:27 +0100

    mdr select with pdo et sqlite3

--- selection.php
+++ selection.php
@@ -1,35 +1,6 @@
 <?php
-
-try {
-    //include 'connection.php';
-    // Create (connect to) SQLite database in file
-    $file_db = new PDO('sqlite:'.dirname(__FILE__).'/bd/participants.sqlite');
-    // Set errormode to exceptions
-    $file_db->setAttribute(PDO::ATTR_ERRMODE,
-                            PDO::ERRMODE_EXCEPTION);
-    //$db = new SQLite3;
-    //
-    // Select all data from file db messages table
-    $resultat = $file_db->query('SELECT * FROM participants');
-    echo $resultat;
-    //$db=sqlite_open('bd/participants.sqlite', 0666, $error);
-    //$db = new PDO('sqlite:'.dirname(__FILE__).'/bd/participants.sqlite');
-    //$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
     
-    //$statement->bindValue(':id', $id);
-
-
-    //préparation de la requete insertion dans la bdd
-    //$statement = $db->prepare("
-    //                        SELECT * FROM participants
-    //                      ");
-    //$resultat = $statement->execute();
-    
-    
-    //$red = $sth->fetchAll();
-    }
-  catch(PDOException $e) {
-    // Print PDOException message
-    echo $e->getMessage();
-  }
+$stma = $pdo->prepare("SELECT * FROM participants");
+$stma->execute();
+$resultat = $stma->fetchAll();
+print_r($resultat);
